1a3b91091b9e47418c873d4ef46301836ad8a543
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-04-08 23:42:08 +0530

    Add files via upload

---
+++ IPC_Q.1(a).c
@@ -0,0 +1,40 @@
+// IPC Assignment //
+// Q.1 //
+// Write a program that creates a shared memory region and displays shared memory id and also
+print address at which shared area in this process starts from. //
+// MAHADEV G I //
+
+//****** Shared memeory read *******//
+
+
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/types.h>
+#include<sys/shm.h>
+#include<sys/stat.h>
+
+int main()
+{
+    char *r;
+    int id;
+   
+    shmat(int id, void *addr, int flags);             // Shmat function declaring
+    
+    id = shmget(48,250,IPC_CREAT|0644);               // Creating the shared memeory id
+    
+    if(id < 0)
+    {
+        perror("shmget\n");
+        return 0;
+    }
+    
+    printf("id = %d\n", id);
+    
+    printf("Address is = %p\n",r);
+    r = shmat(id,0,0);                      // Returning an shmat value 'r' to get the id and address
+    
+    printf("%s\n",r);
+    
+    
+    return 0;
+}    
---
+++ IPC_Q.1(b).c
@@ -0,0 +1,37 @@
+// IPC Assignment //
+// Q.1 //
+// write a program that creates a shared memory region and displays shared memory id and also
+print address at which shared area in this process starts from. //
+// MAHADEV G I //
+
+// ******Shared memeory write ****** //
+// 
+
+
+
+
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/types.h>
+#include<sys/shm.h>
+
+int main()
+{
+    char *p;
+    int id;
+    
+    id = shmget(48,250,IPC_CREAT|0644);              // Shmget function to get the shmid
+    
+    if(id<0)
+    {
+        perror("shmget");
+        return 0;
+    }
+    
+    printf("id = %d\n", id);
+    p = shmat(id,0,0);                             // Shmat function used to write in the data
+    printf("Enter the data:");
+    scanf("%s", p);
+    
+    return 0;
+}        
